7f222ebb05cd97f11ae3c159ea66c7fb5ddb6685
Gabriel Mango <gabrielhmango@gmail.com>
2023-09-29 16:36:36 -0300

    Consulta dados duplicados

--- conexao.py
+++ conexao.py
@@ -3,28 +3,9 @@ from dotenv import load_dotenv
 from sqlalchemy import create_engine, func
 from sqlalchemy.orm import sessionmaker
 
-from database import Documento
-
 
 load_dotenv()
 URL_DATABASE = os.getenv('URL_DATABASE')
 engine = create_engine(URL_DATABASE)
 Session = sessionmaker(bind=engine)
 session = Session()
-
-
-
-if __name__ == '__main__':
-
-    query = session.query(Documento.nu_documento, Documento.tp_documento, func.count().label('count')) \
-        .filter(Documento.tp_documento == 'IDENTIDADE') \
-        .group_by(Documento.nu_documento, Documento.tp_documento) \
-        .having(func.count() > 1) \
-        .order_by(Documento.nu_documento)
-
-    results = query.all()
-
-    for result in results:
-        print(f'nu_documento: {result.nu_documento}, tp_documento: {result.tp_documento}, count: {result.count}')
-
-    session.close()
